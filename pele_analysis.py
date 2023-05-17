bd3e868f84713f57abc275dbb33031f2c902d1c2
IFilella <isaacfilella@gmail.com>
2023-05-17 14:30:51 +0200

    pele_analysis flag update

--- pele_analysis.py
+++ pele_analysis.py
@@ -16,15 +16,24 @@ if __name__ == '__main__':
     parser.add_argument('-o', dest="outname", help = "Outname",required=True)
     parser.add_argument('--glide',dest='glide',help='Glide docking csv file', default=None)
     parser.add_argument('--PELEmetric',dest='metric',help='Final PELE metric',default='all_BFE')
-    parser.add_argument('--charge',dest='charge',help='if added compute the net charge of each molecule (only works if glide.csv with smiles is provided)', default=False,action='store_true')
+    parser.add_argument('--charge',dest='charge',help='if added compute the net charge of each molecule (only works if glide.csv with SMILES is provided)', default=False,action='store_true')
+    parser.add_argument('--maxtanimoto',dest='maxtanimoto',help='add it if maxtanimoto property included in the glide.csv', default=False,action='store_true')
+    parser.add_argument('--batch',dest='batch',help='add it if batch property included in the glide.csv', default=False,action='store_true')
+    parser.add_argument('--smile',dest='smile',help='add it if SMILE property included in the glide.csv', default=False,action='store_true')
+
     args = parser.parse_args()
 
     pele_output = args.RESdir
+    if pele_output[-1] != '/': pele_output += '/'
     ligs_dir = args.LIGSdir
+    if ligs_dir[-1] != '/': ligs_dir += '/'
     outname = args.outname
     glide_csv = args.glide
     metric = args.metric
     charge = args.charge
+    maxtanimoto = args.maxtanimoto
+    batch = args.batch
+    smile = args.smile
 
     molecules_out = glob.glob('%s/*/'%pele_output)
     molecules_out = [molecule_out for molecule_out in molecules_out if '_min' not in molecule_out]
@@ -32,14 +41,19 @@ if __name__ == '__main__':
 
     #Get a .csv of all PELE simulations with PELE metrics and if required glide gscore
     f_out =  open('%s_pelemetrics.csv'%outname,'w')
+    header = 'molecule,smile,batch,maxtanimoto,charge,glidegscore,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE'
     if glide_csv:
-        if charge:
-            header = 'molecule,SMILE,batch,maxtanimoto,glidegscore,charge,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE'
-        else:
-            header = 'molecule,SMILE,batch,maxtanimoto,glidegscore,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE'
+        if not smile:
+            header = header.replace('smile,','')
+        if not charge:
+            header = header.replace('charge,','')
+        if not batch:
+            header = header.replace('batch,','')
+        if not maxtanimoto:
+            header = header.replace('maxtanimoto,','')
         glide_df = pd.read_csv(glide_csv)
     else:
-        header = 'molecule,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE'
+        header = header.replace('smile,','').replace('charge,','').replace('batch,','').replace('maxtanimoto,','')
     f_out.write(header+'\n')
     for i,molecule in enumerate(molecules):
         print('-', i+1, molecule)
@@ -51,9 +65,12 @@ if __name__ == '__main__':
             else:
                 row = glide_df[(glide_df['Title'] == molecule)]
             glidegscore = row['glide gscore'].min()
-            maxtanimoto = row['maxtanimoto'].max()
-            smile = row['smiles'].iloc[0]
-            batch = row['batch'].iloc[0]
+            if maxtanimoto:
+                maxt = row['maxtanimoto'].max()
+            if smile:
+                sml = row['smiles'].iloc[0]
+            if batch:
+                bat = row['batch'].iloc[0]
             if charge:
                 mol = mollib.Mol(smile=smile)
                 netcharge = Chem.rdmolops.GetFormalCharge(mol.mol)
@@ -88,23 +105,30 @@ if __name__ == '__main__':
         lowBFE_dict_key = list(lowBFE_dict.keys())[0]
         lowBFE_avgBE, lowBFE_minBE, lowBFE_BZM, lowBFE_BFE = lowBFE_dict[lowBFE_dict_key]
 
+        f_out.write('%s,'%molecule)
         if glide_csv:
-            if charge:
-                f_out.write('%s,%s,%s,%.4f,%.4f,%d,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(molecule,smile,batch,maxtanimoto,glidegscore,netcharge,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE))
-            else:
-                f_out.write('%s,%s,%s,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(molecule,smile,batch,maxtanimoto,glidegscore,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE))
+            if smile: f_out.write('%s,'%sml)
+            if batch: f_out.write('%s,'%bat)
+            if maxtanimoto: f_out.write('%.4f,'%maxt)
+            if charge: f_out.write('%d,'%netcharge)
+            f_out.write('%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(glidegscore,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE))
         else:
-            f_out.write('%s,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(molecule,all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE))
+            f_out.write('%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(all_avgBE,all_minBE,all_BZM,all_BFE,pop_avgBE,pop_minBE,pop_BZM,pop_BFE,lowavg_avgBE,lowavg_minBE,lowavg_BZM,lowavg_BFE,lowBE_avgBE,lowBE_minBE,lowBE_BZM,lowBE_BFE,lowBZM_avgBE,lowBZM_minBE,lowBZM_BZM,lowBZM_BFE,lowBFE_avgBE,lowBFE_minBE,lowBFE_BZM,lowBFE_BFE))
     f_out.close()
 
     #Get final csv file with selected PELE metric and final poses (with all_lowBE)
     f_in = open('%s_pelemetrics.csv'%outname,'r')
     f_out = open('%s.csv'%outname,'w')
     if glide_csv:
-        if charge:
-            header = 'molecule,smile,batch,maxtanimoto,glidegscore,charge,all_minBE,%s\n'%metric
-        else:
-            header = 'molecule,smile,batch,maxtanimoto,glidegscore,all_minBE,%s\n'%metric
+        header = 'molecule,smile,batch,maxtanimoto,charge,glidegscore,all_minBE,%s\n'%metric
+        if not smile:
+            header = header.replace('smile,','')
+        if not charge:
+            header = header.replace('charge,','')
+        if not batch:
+            header = header.replace('batch,','')
+        if not maxtanimoto:
+            header = header.replace('maxtanimoto,','')
     else:
         header = 'molecule,all_minBE,%s\n'%metric
     f_out.write(header)
@@ -118,16 +142,22 @@ if __name__ == '__main__':
         if i==0:
             idx_metric = line.index(metric)
             idx_minBE = line.index('all_minBE')
+            if smile: idx_smile = line.index('smile')
+            if batch: idx_batch = line.index('batch')
+            if maxtanimoto: idx_maxtanimoto = line.index('maxtanimoto')
+            if charge: idx_charge = line.index('charge')
+            idx_glidegscore = line.index('glidegscore')
             continue
         molecule = line[0]
         print('-', i, molecule)
+        f_out.write('%s,'%molecule)
         if glide_csv:
-            if charge:
-                f_out.write('%s,%s,%s,%s,%s,%s,%s,%s\n'%(molecule,line[1],line[2],line[3],line[4],line[5],line[idx_minBE],line[idx_metric]))
-            else:
-                f_out.write('%s,%s,%s,%s,%s,%s,%s\n'%(molecule,line[1],line[2],line[3],line[4],line[idx_minBE],line[idx_metric]))
-        else:
-            f_out.write('%s,%s,%s\n'%(molecule,line[idx_minBE],line[idx_metric]))
+            if smile: f_out.write('%s,'%line[idx_smile])
+            if batch: f_out.write('%s,'%line[idx_batch])
+            if maxtanimoto: f_out.write('%s,'%line[idx_maxtanimoto])
+            if charge: f_out.write('%s,'line[idx_charge])
+            f_out.write('%s,'%line[idx_glidegscore])
+        f_out.write('%s,%s\n'%(line[idx_minBE],line[idx_metric]))
 
         #Get poses
         poses_path = '%s%s/analysis/top_poses'%(pele_output,molecule)
