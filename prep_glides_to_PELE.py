4ba4a4841bc8d50109bbb559b305c78b2a0e66d7
IFilella <isaacfilella@gmail.com>
2024-06-04 14:25:56 +0200

    removed old glide filtering options

--- prep_glides_to_PELE.py
+++ prep_glides_to_PELE.py
@@ -4,9 +4,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description ='Given a directory with multiple compounds (LIGSdir) docked to a '
                                                   'given target (target_pdb) using Glide, prepare them for a PELE '
-                                                  'simulation. Docked compounds can be filtered by a Glide feature'
-                                                  ' (feature_filter and value_filter) when a Glide table is provided'
-                                                  ' (csv).')
+                                                  'simulation.')
     requiredArguments = parser.add_argument_group('required arguments')
     requiredArguments.add_argument('--LIGSdir', dest="LIGSdir", help = "Directory with the docked compounds (This must be in PDB format)",required=True)
     requiredArguments.add_argument('--target_pdb', dest="target_pdb", help = "Target PDB",required=True)
@@ -14,14 +12,6 @@ if __name__ == '__main__':
                                                      "for the PELE simulation"
                         ,required=True)
     parser.add_argument('--HBlist',dest='HBlist',help='Generate a list of all Hydrogen Bonds of the complex',action='store_true',default=False)
-    parser.add_argument('--csv',dest="csv",help="Glide feature table",default=None)
-    parser.add_argument('--feature_filter',dest="feature_filter",help="Glide feature to apply a compound filter"
-                        ,default=None)
-    parser.add_argument('--value_filter',dest="value_filter",help="Value to which apply the glide feature filter"
-                        ,default=None)
-    parser.add_argument('--sdf_out',dest="sdf_out",help="Output sdf file with the molecules fullfiling the Glide feature"
-                                                        " filter"
-                       ,default=None)
     requiredArguments.add_argument('--partition', dest='partition', help='MN5 partition either gpp or acc', required=True)
 
     args = parser.parse_args()
@@ -34,19 +24,6 @@ if __name__ == '__main__':
     LIGSdir = current_path + '/' + args.LIGSdir
     target_pdb = current_path + '/' + args.target_pdb
     complexname = args.outname
-    if args.csv:
-        data = current_path + '/' + args.csv
-    else:
-        data = args.csv
-    feature_filter = args.feature_filter
-    if args.value_filter:
-        value_filter = float(args.value_filter)
-    else:
-        value_filter = args.value_filter
-    if args.sdf_out:
-        sdf_out = current_path + '/' + args.sdf_out
-    else:
-        sdf_out = args.sdf_out
     HBlist = args.HBlist
     partition = args.partition
     if partition == 'gpp':
@@ -59,14 +36,6 @@ if __name__ == '__main__':
     cmd = 'python scripts/prep_files.py --LIGSdir %s --target_pdb %s -o %s' % (LIGSdir, target_pdb, complexname)
     if HBlist:
         cmd += ' --HBlist'
-    if data:
-        cmd += ' --csv %s' % data
-    if value_filter:
-        cmd += ' --value_filter %s' % value_filter
-    if feature_filter:
-        cmd += ' --feature_filter %s' % feature_filter
-    if sdf_out:
-        cmd += ' --sdf_out %s' % sdf_out
 
     with open('prepligs.sh', 'w') as fileout:
         fileout.writelines(
