3e81a17671e29b24258455d572d45eb1d426a342
IFilella <isaacfilella@gmail.com>
2024-06-07 11:29:00 +0200

    output path changed due to suite implementation

--- scripts/generate_results.py
+++ scripts/generate_results.py
@@ -14,7 +14,9 @@ if __name__ == '__main__':
     originalDIR = os.getcwd()
 
     os.chdir(workDIR)
-    topology = glob.glob('%s/input/*processed*'%workDIR)[0]
+    path = '/'.join(workDIR.split('/')[:-2])
+    path = os.path.join(path,'2_pele_pdb_preprocessor/complexes')
+    topology = path + '/complex_2.pdb'
     analysis = Analysis(resname="LIG", chain="L", simulation_output="output",traj='trajectory.xtc',topology=topology,be_column=4,report="report",cpus=32)
-    analysis.generate(path="results", clustering_type="meanshift")
+    analysis.generate(path="analysis", clustering_type="meanshift")
     os.chdir(originalDIR)
